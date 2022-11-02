d6ab6b55753f29bd3e17f569bb51ba1b1fc0b22d
Naomi Clark <nclark921@west-mec.org>
2022-11-02 13:10:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, } from 'react';
 function App() {
   const [quote, setQuote] = useState('')
-  const [random, setRandom] = useState(0)~
+  const [random, setRandom] = useState(0)
   async function fetchQuote() {
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
       .then(response => response.json())
